d3d7d31c641d199e77ba61d46582a1ad1e7a3e49
TarnamGoel <tarnamgoel@gmail.com>
2023-10-09 19:43:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from './pages/Nav';
+import About from './pages/About'
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import app from './styles/App.css';
+import Background from "./pages/Background";
+import PlayerStats from "./pages/PlayerStats";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return <>
+    <Router>
+      <Nav></Nav>
+      <Background/>
+        <Routes>
+          <Route path="/" element={<About />}></Route>
+          <Route path="/skills" element={<Skills />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+        <PlayerStats/>
+    </Router>
+  </>;
 }
 
 export default App;
